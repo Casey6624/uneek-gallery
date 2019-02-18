a4d9a78b33cc2deaf60429e7181ed42d265f5f44
Casey6624 <34159052+Casey6624@users.noreply.github.com>
2019-02-18 19:02:26 +0000

    Backup before removing Widget endpoint and container

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,11 @@ const path = require('path');
 module.exports = (env, argv) => {
   let production = argv.mode === 'production'
 
+  new webpack.DefinePlugin({
+    'process.env.NODE_ENV': JSON.stringify('production')
+  }),
+  new webpack.optimize.UglifyJsPlugin()
+  
   return {
     entry: {
       'js/admin': path.resolve(__dirname, 'app/admin.js'),
