954d359a22dfe1e49ec14b2910092c66ddb6ec36
Casey6624 <34159052+Casey6624@users.noreply.github.com>
2019-04-11 21:39:52 +0100

    AllPosts.jsx is now completely written in hooks. Next i will implement chained axios.all calls

--- app/components/AllPosts/AllPosts.jsx
+++ app/components/AllPosts/AllPosts.jsx
@@ -1,36 +1,34 @@
+/* eslint-disable react/prop-types */
 // Libaries
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 // Components
 import Loading from "../Loading/Loading";
 import Post from "../Post/Post";
 import SearchBar from "../SearchBar/SearchBar";
-import { throws } from "assert";
 
-export default class AllPosts extends Component{
+export default function AllPosts(props){
 
-    state = {
-        postData: [],
-        dataFetched: false,
-        filterValue: "",
-        categoryData: null,
-        categoryFilter: null,
-        categoryFilterNoResults: false,
-        activeCategory: "ALL",
-        parentCategory: null
-    } 
+    let [postData, setPostData] = useState([])
+    let [dataFetched, setDataFetched] = useState(false)
+    let [filterValue, setFilterValue] = useState("")
+    let [categoryData, setCategoryData] = useState(null)
+    let [categoryFilter, setCategoryFilter] = useState(null)
+    let [categoryFilterNoResults, setCategoryFilterNoResults] = useState(false)
+    let [activeCategory, setActiveCategory] = useState("ALL")
+    let [parentCategory, setParentCategory] = useState(null)
 
-    getParentCategory = () => {
-        const { api_url, categoryToRender: categoryID } = this.props;
+    function getParentCategory(){
+        const { api_url, categoryToRender: categoryID } = props;
         const trimmedURL = api_url.split("wp-json")[0];
         axios.get(`${trimmedURL}wp-json/wp/v2/categories/${categoryID}`)
         .then( res => {
-            this.setState({ parentCategory: res.data.name })
+            setParentCategory(res.data.name)
         })
     }
 
-    getCategories = () => {
-        const { api_url, categoryToRender: categoryID } = this.props;
+    function getCategories(){
+        const { api_url, categoryToRender: categoryID } = props;
         const trimmedURL = api_url.split("wp-json")[0];
         let fetchURL = `${trimmedURL}wp-json/wp/v2/categories?parent=${categoryID}`
         axios.get(fetchURL)
@@ -40,39 +38,38 @@ export default class AllPosts extends Component{
                 if(res.data[i].name === "Uncategorised") continue;
                 categoryData[res.data[i].id] = res.data[i].name; 
             }
-            this.setState({ categoryData })
+            setCategoryData(categoryData)
         })
-        this.getParentCategory();
+        getParentCategory();
     }
 
     // call API, get posts and store in state
-    getPosts = () => {
-        const { api_url, categoryToRender: categoryID } = this.props;
+    function getPosts(){
+        const { api_url, categoryToRender: categoryID } = props;
         const trimmedURL = api_url.split("wp-json")[0];
         const fetchURL = `${trimmedURL}wp-json/wp/v2/posts?categories=${categoryID}&_embed`;
         axios.get(fetchURL)
         .then(res => {
-            this.setState({
-                postData: res.data,
-                dataFetched: true,
-            })
+            setPostData(res.data)
+            setDataFetched(true)
         })
     }    
-
-    componentDidMount(){
+    // Essentially a componentDidMount()
+    useEffect(() => {
         document.addEventListener("keydown", (event) =>{
             if(event.key === "Escape"){
-                this.handleClearSearchBox(event);
+                handleClearSearchBox(event);
             }
         } )
-    }
-    // attached to the escape key using componentDidMount()
-    handleClearSearchBox = () => this.setState({filterValue: ""})
+    }, [])
 
-    stripHTML = indexOrData => {
+    // attached to the escape key using componentDidMount()
+    function handleClearSearchBox(){setFilterValue("")}
+
+    function stripHTML(indexOrData){
         let rawData;
         if(Number.isInteger(indexOrData)){
-            rawData = this.state.postData[indexOrData].excerpt.rendered;
+            rawData = postData[indexOrData].excerpt.rendered;
         }else{rawData = indexOrData}
         return new DOMParser()
           .parseFromString(rawData, 'text/html')
@@ -81,35 +78,33 @@ export default class AllPosts extends Component{
           .trim()
       }
 
-      assignCategories = index => {
-        let catFromState = this.state.postData[index].categories;
+      function assignCategories(index){
+        let catFromState = postData[index].categories
         let categoryNames = "";
         for(let i = 0; i < catFromState.length; i++){
-            if(this.state.categoryData[catFromState[i]] == this.state.parentCategory) continue
-            if(this.state.categoryData[catFromState[i]] == undefined) continue
-            categoryNames += this.state.categoryData[catFromState[i]];
+            if(categoryData[catFromState[i]] == parentCategory) continue
+            if(categoryData[catFromState[i]] == undefined) continue
+            categoryNames += categoryData[catFromState[i]];
         }
         return categoryNames;
     }
 
-    paginationToggled = event => {let el = event.target.name;console.log(`${el} has been fired!`)}
-    
-    filterChangeHandler = e => this.setState({ filterValue: e.target.value.toUpperCase()});
+    function paginationToggled(event){ let el = event.target.name }
+
+    function filterChangeHandler(e) {setFilterValue(e.target.value.toUpperCase())}
 
     // TEXT SEARCH
-    filterItems = () =>{
-    const { postData, filterValue } = this.state;
-    let sortedFilteredPosts = postData.filter(({ title }) => title.rendered.includes(filterValue));
-    return sortedFilteredPosts;
+    function filterItems(){
+        let sortedFilteredPosts = postData.filter(({ title }) => title.rendered.includes(filterValue));
+        return sortedFilteredPosts;
     }
     // CATEGORY <a> TAGS </a> SEARCH
-    categoryFilterItems = () =>{
-        let { postData, categoryFilter } = this.state;
+    function categoryFilterItems(){
         let filteredCategories = postData.filter(category =>{
             // making this [0] fixes crowd funding category filter
             let categoryFinal;
             for(let i = 0; i < category.categories.length; i++){
-                if (category.categories[i] == this.props.categoryToRender) continue
+                if (category.categories[i] == props.categoryToRender) continue
                 if (category.categories[i] == undefined) continue
                 categoryFinal = category.categories[i]
             }
@@ -118,24 +113,24 @@ export default class AllPosts extends Component{
         return filteredCategories
     }
 
-    categoryFilterHandler = event => {
+    function categoryFilterHandler(event){
         let element = event.target.name
         // if null name then use value (for dropdown list)
         if(!element) element = event.target.value
-        let catStateObj = this.state.categoryData;
+        let catStateObj = categoryData;
         let catId = Object.keys(catStateObj).find(key  => catStateObj[key] === element)
         if(element === "ALL") catId = null;
-        this.setState({categoryFilter: catId, activeCategory: element})
+        setCategoryFilter(catId)
+        setActiveCategory(element)
     }
 
-render(){
-    if(!this.state.dataFetched && this.props.categoryToRender !== null){
-        this.getCategories();
-        this.getPosts();
+    if(!dataFetched && props.categoryToRender !== null){
+        getCategories();
+        getPosts();
         return(<div><Loading /></div>)
     }
 
-    if(this.state.dataFetched && this.state.postData.length === 0){
+    if(dataFetched && postData.length === 0){
         return(
             <div className="noPostsFound">
                 <h1 >No Posts Found! :(</h1>
@@ -144,77 +139,75 @@ render(){
         )
     }
 
-    if(this.state.filterValue !== ""){
+    if(filterValue !== ""){
         return(
             <React.Fragment>
-                <SearchBar focus value={this.state.filterValue} onChange={this.filterChangeHandler} />
+                <SearchBar focus value={filterValue} onChange={filterChangeHandler} />
             {/* filtered film results */}
-            {this.filterItems().map((post, index) => <Post
-                key={this.filterItems()[index].id === undefined ? null : this.filterItems()[index].id}
-                filmTitle={this.filterItems()[index].title.rendered === undefined ? null : this.filterItems()[index].title.rendered.toUpperCase()}
-                filmExcerpt={this.stripHTML(this.filterItems()[index]) === undefined ? null : this.stripHTML(this.filterItems()[index].excerpt.rendered)}  
-                filmImage={this.filterItems()[index]._embedded['wp:featuredmedia'] === undefined ? null : this.filterItems()[index]._embedded['wp:featuredmedia'][0].source_url}
-                filmLink={this.filterItems()[index].link === undefined ? null : this.filterItems()[index].link}
-                filmCategories={this.assignCategories(index) === undefined ? null : this.assignCategories(index)}
+            {filterItems().map((post, index) => <Post
+                key={filterItems()[index].id === undefined ? null : filterItems()[index].id}
+                filmTitle={filterItems()[index].title.rendered === undefined ? null : filterItems()[index].title.rendered.toUpperCase()}
+                filmExcerpt={stripHTML(filterItems()[index]) === undefined ? null : stripHTML(filterItems()[index].excerpt.rendered)}  
+                filmImage={filterItems()[index]._embedded['wp:featuredmedia'] === undefined ? null : filterItems()[index]._embedded['wp:featuredmedia'][0].source_url}
+                filmLink={filterItems()[index].link === undefined ? null : filterItems()[index].link}
+                filmCategories={assignCategories(index) === undefined ? null : assignCategories(index)}
             />)}
-            {<h4 className="resultsFoundText">{`${this.filterItems().length} ${this.filterItems().length === 1 ? "RESULT" : "RESULTS"} FOUND`}</h4>}
+            {<h4 className="resultsFoundText">{`${filterItems().length} ${filterItems().length === 1 ? "RESULT" : "RESULTS"} FOUND`}</h4>}
             </React.Fragment>
         )
     }
 
-    if(this.state.categoryFilter !== null){
+    if(categoryFilter !== null){
         return(
             <React.Fragment>
-                    <SearchBar focus value={this.state.filterValue} onChange={this.filterChangeHandler}/>
+                    <SearchBar focus value={filterValue} onChange={filterChangeHandler}/>
                 <div className="categoryLinks"><h3 id="filterByStageLabel">FILTER BY STAGE: </h3>
-            {this.state.categoryData === null ? null : Object.keys(this.state.categoryData).map((key, index) => 
-                <a className={this.state.activeCategory == this.state.categoryData[key] ? "categoryLinkActive" : "categoryLink"} onClick={this.categoryFilterHandler} key={this.state.categoryData[key]} name={this.state.categoryData[key]}>{this.state.categoryData[key]}</a>)}
+            {categoryData === null ? null : Object.keys(categoryData).map((key, index) => 
+                <a className={activeCategory == categoryData[key] ? "categoryLinkActive" : "categoryLink"} onClick={categoryFilterHandler} key={categoryData[key]} name={categoryData[key]}>{categoryData[key]}</a>)}
             
-            <select className="categoryLinksDropDown" onChange={this.categoryFilterHandler}>
-            {this.state.categoryData === null ? null : Object.keys(this.state.categoryData).map((key, index) => 
-                <option key={this.state.categoryData[key]} value={this.state.categoryData[key]} name={this.state.categoryData[key]}>{this.state.categoryData[key]}</option>
+            <select className="categoryLinksDropDown" onChange={categoryFilterHandler}>
+            {categoryData === null ? null : Object.keys(categoryData).map((key, index) => 
+                <option key={categoryData[key]} value={categoryData[key]} name={categoryData[key]}>{categoryData[key]}</option>
                 )}
             </select>
             </div>
             {/* filtered film results */}
-            {this.categoryFilterItems().map((post, index) => <Post
-                key={this.categoryFilterItems()[index].id === undefined ? null : this.categoryFilterItems()[index].id}
-                filmTitle={this.categoryFilterItems()[index].title.rendered === undefined ? null : this.categoryFilterItems()[index].title.rendered.toUpperCase()}
-                filmExcerpt={this.stripHTML(this.categoryFilterItems()[index]) === undefined ? null : this.stripHTML(this.categoryFilterItems()[index].excerpt.rendered)}  
-                filmImage={this.categoryFilterItems()[index]._embedded['wp:featuredmedia'] === undefined ? null : this.categoryFilterItems()[index]._embedded['wp:featuredmedia'][0].source_url}
-                filmLink={this.categoryFilterItems()[index].link === undefined ? null : this.categoryFilterItems()[index].link}
-                //filmCategories={this.assignCategories(index) === undefined ? null : this.assignCategories(this.categoryFilterItems()[index])}
+            {categoryFilterItems().map((post, index) => <Post
+                key={categoryFilterItems()[index].id === undefined ? null : categoryFilterItems()[index].id}
+                filmTitle={categoryFilterItems()[index].title.rendered === undefined ? null : categoryFilterItems()[index].title.rendered.toUpperCase()}
+                filmExcerpt={stripHTML(categoryFilterItems()[index]) === undefined ? null : stripHTML(categoryFilterItems()[index].excerpt.rendered)}  
+                filmImage={categoryFilterItems()[index]._embedded['wp:featuredmedia'] === undefined ? null : categoryFilterItems()[index]._embedded['wp:featuredmedia'][0].source_url}
+                filmLink={categoryFilterItems()[index].link === undefined ? null : categoryFilterItems()[index].link}
+                //filmCategories={assignCategories(index) === undefined ? null : assignCategories(categoryFilterItems()[index])}
             />)}
-            {<h4 className="resultsFoundText">{`${this.categoryFilterItems().length} ${this.categoryFilterItems().length === 1 ? "RESULT" : "RESULTS"} FOUND FOR ${this.state.activeCategory}`}</h4>}
+            {<h4 className="resultsFoundText">{`${categoryFilterItems().length} ${categoryFilterItems().length === 1 ? "RESULT" : "RESULTS"} FOUND FOR ${activeCategory}`}</h4>}
             </React.Fragment>
         )
     }
 
     return(
         <React.Fragment>
-        {<SearchBar focus value={this.state.filterValue} onChange={this.filterChangeHandler}/>}
+        {<SearchBar focus value={filterValue} onChange={filterChangeHandler}/>}
         <div className="categoryLinks"><h3 id="filterByStageLabel">FILTER BY STAGE: </h3>
-            {this.state.categoryData === null ? null : Object.keys(this.state.categoryData).map((key, index) => 
-                <a className={this.state.activeCategory == this.state.categoryData[key] ? "categoryLinkActive" : "categoryLink"} onClick={this.categoryFilterHandler} key={this.state.categoryData[key]} name={this.state.categoryData[key]}>{this.state.categoryData[key]}</a>)}
+            {categoryData === null ? null : Object.keys(categoryData).map((key, index) => 
+                <a className={activeCategory == categoryData[key] ? "categoryLinkActive" : "categoryLink"} onClick={categoryFilterHandler} key={categoryData[key]} name={categoryData[key]}>{categoryData[key]}</a>)}
             
-            <select className="categoryLinksDropDown" onChange={this.categoryFilterHandler}>
-            {this.state.categoryData === null ? null : Object.keys(this.state.categoryData).map((key, index) => 
-                <option key={this.state.categoryData[key]} value={this.state.categoryData[key]} name={this.state.categoryData[key]}>{this.state.categoryData[key]}</option>
+            <select className="categoryLinksDropDown" onChange={categoryFilterHandler}>
+            {categoryData === null ? null : Object.keys(categoryData).map((key, index) => 
+                <option key={categoryData[key]} value={categoryData[key]} name={categoryData[key]}>{categoryData[key]}</option>
                 )}
             </select>
 
             </div>
-        {this.state.postData.map((post, index) => <Post
-            key={this.state.postData[index].id === undefined ? null : this.state.postData[index].id}
-            filmTitle={this.state.postData[index].title.rendered === undefined ? null : this.state.postData[index].title.rendered.toUpperCase()}
-            filmExcerpt={this.stripHTML(index) === undefined ? null : this.stripHTML(index)}    
-            filmImage={this.state.postData[index]._embedded['wp:featuredmedia'] === undefined ? null : this.state.postData[index]._embedded['wp:featuredmedia'][0].source_url}
-            filmLink={this.state.postData[index].link === undefined ? null : this.state.postData[index].link}
-            filmCategories={this.assignCategories(index) === undefined ? null : this.assignCategories(index)}
+        {postData.map((post, index) => <Post
+            key={postData[index].id === undefined ? null : postData[index].id}
+            filmTitle={postData[index].title.rendered === undefined ? null : postData[index].title.rendered.toUpperCase()}
+            filmExcerpt={stripHTML(index) === undefined ? null : stripHTML(index)}    
+            filmImage={postData[index]._embedded['wp:featuredmedia'] === undefined ? null : postData[index]._embedded['wp:featuredmedia'][0].source_url}
+            filmLink={postData[index].link === undefined ? null : postData[index].link}
+            filmCategories={assignCategories(index) === undefined ? null : assignCategories(index)}
         />)}
-        {<h4 className="resultsFoundText">{`${this.state.postData.length} ${this.state.postData.length === 1 ? "RESULT" : "RESULTS"} FOUND FOR ${this.state.activeCategory}`}</h4>}
+        {<h4 className="resultsFoundText">{`${postData.length} ${postData.length === 1 ? "RESULT" : "RESULTS"} FOUND FOR ${activeCategory}`}</h4>}
         </React.Fragment>
     )
-}
-
 }
